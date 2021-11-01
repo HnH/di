f0cdbacf00f188e4808a3c537ab75ef8e6fb4f72
Sergey Treinis <sergey@treinis.ru>
2021-11-01 13:09:23 +0300

    Option comments

--- opts.go
+++ opts.go
@@ -1,23 +1,29 @@
 package di
 
+// Option represents single option type
 type Option func(Options)
 
+// Options represents a target for applying an Option
 type Options interface {
 	Apply(Option)
 }
 
+// NamingOption supports setting a name
 type NamingOption interface {
 	SetName(...string)
 }
 
+// ReturnOption supports setting a return target
 type ReturnOption interface {
 	SetReturn(...interface{})
 }
 
+// FillingOption supports setting a fill flag
 type FillingOption interface {
 	SetFill(bool)
 }
 
+// WithName returns a NamingOption
 func WithName(names ...string) Option {
 	return func(o Options) {
 		if opt, ok := o.(NamingOption); ok {
@@ -26,6 +32,7 @@ func WithName(names ...string) Option {
 	}
 }
 
+// WithReturn returns a ReturnOption
 func WithReturn(returns ...interface{}) Option {
 	return func(o Options) {
 		if opt, ok := o.(ReturnOption); ok {
@@ -34,6 +41,7 @@ func WithReturn(returns ...interface{}) Option {
 	}
 }
 
+// WithFill returns a FillingOption
 func WithFill() Option {
 	return func(o Options) {
 		if opt, ok := o.(FillingOption); ok {
@@ -57,14 +65,17 @@ func newBindOptions(opts []Option) (out bindOptions) {
 	return
 }
 
+// Apply implements Options interface
 func (self *bindOptions) Apply(opt Option) {
 	opt(self)
 }
 
+// SetName implements NamingOption interface
 func (self *bindOptions) SetName(names ...string) {
 	self.names = names
 }
 
+// SetFill implements FillingOption interface
 func (self *bindOptions) SetFill(f bool) {
 	self.fill = f
 }
@@ -83,10 +94,12 @@ func newResolveOptions(opts []Option) (out resolveOptions) {
 	return
 }
 
+// Apply implements Options interface
 func (self *resolveOptions) Apply(opt Option) {
 	opt(self)
 }
 
+// SetName implements NamingOption interface
 func (self *resolveOptions) SetName(names ...string) {
 	if len(names) > 0 {
 		self.name = names[0]
@@ -106,10 +119,12 @@ func newCallOptions(opts []Option) (out callOptions) {
 	return
 }
 
+// Apply implements Options interface
 func (self *callOptions) Apply(opt Option) {
 	opt(self)
 }
 
+// SetReturn implements ReturnOption interface
 func (self *callOptions) SetReturn(returns ...interface{}) {
 	self.returns = returns
 }
